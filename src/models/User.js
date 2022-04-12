9400be00cd427a777d7a1eed03caafc8466bac53
Victor Bergamini <vbergdev@gmail.com>
2022-04-11 22:11:56 -0300

    feat: User model and test.js

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  cpf: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  active: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  }
+})
+
+module.exports = User;
