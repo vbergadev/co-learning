0d3157effb7ea424cbe34a531462175a0f42a738
Victor Bergamini <vbergdev@gmail.com>
2022-04-11 22:12:51 -0300

    config Sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
-module.exports = {
-  dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password:"Info@1234",
-  database: "dhbank"
-}
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('dhbank', 'root', 'Info@1234', {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: '3306'
+});
+
+module.exports = sequelize;
